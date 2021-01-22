96bcd5325bd505e6513bc3022073080be4289643
js-yu1592 <wnstkd311@daum.net>
2021-01-22 22:47:10 +0900

    TabBar Controller

---
+++ TabBarControllerApp/TabBarControllerApp/MainTabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TabBarControllerApp
+//
+//  Created by 유준상 on 2020/12/01.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

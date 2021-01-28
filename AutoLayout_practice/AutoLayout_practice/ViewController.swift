0a322e1d2bbdb9bfbda4fba95aa38fbb5497a85b
js-yu1592 <wnstkd311@daum.net>
2021-01-28 23:16:09 +0900

    Autolayout

---
+++ AutoLayout_practice/AutoLayout_practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayout_practice
+//
+//  Created by 유준상 on 2021/01/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
